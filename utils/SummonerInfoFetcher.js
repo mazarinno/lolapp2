1fd2b0d1faa8af908ba43da1695efcb7dc76b08e
mazarinno <mcsfakianos@gmail.com>
2022-06-18 12:58:40 -0500

    xp total

--- utils/SummonerInfoFetcher.js
+++ utils/SummonerInfoFetcher.js
@@ -233,6 +233,8 @@ define([
       let killCount = 0;
       let deathCount = 0;
       let assistCount = 0;
+      let xpTotal = 0;
+      let cumulativeXP = [0, 280, 660, 1140, 1720, 2400, 3180, 4060, 5040, 6120, 7300, 8580, 9960, 11440, 13020, 14700, 16480, 18360]
 
       // TODO change these globals into local inside this gameflow if statement
       // fetch player team
@@ -258,9 +260,11 @@ define([
             assistCount += player.scores.assists;
 
             // use to loop to also increment levelcount and figure out an equation to convert this to total xp of the group
-
+            xpTotal += cumulativeXP[player.level - 1];
           }
         }
+
+
 
         // TODO pass these variables out to a csv ...
 
